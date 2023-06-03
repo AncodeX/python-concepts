336fb940f462624796706b851891ad5acae7397e
Daniel Ramirez <dramirezn18@gmail.com>
2023-06-03 10:25:36 -0500

    reto 12 y 13 sin resolver

---
+++ retos/request/12_reto.py
@@ -0,0 +1,5 @@
+"""
+Lee esta url y encuentra las 10 palabras más frecuentes. 
+
+    romeo_y_julieta = ' http://www.gutenberg.org/files/1112/1112.txt '
+"""
---
+++ retos/request/13_reto.py
@@ -0,0 +1,6 @@
+"""
+Lea la API de gatos y cats_api = ' https://api.thecatapi.com/v1/breeds ' y busque:
+    - el mínimo, máximo, media, mediana, desviación estándar del peso de los gatos en unidades métricas.
+    - el mínimo, máximo, media, mediana, desviación estándar de la vida útil de los gatos en años.
+    - Crear una tabla de frecuencias de país y raza de gatos.
+"""
